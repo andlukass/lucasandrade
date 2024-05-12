e914635cac290485cd8dd3885628c08c82c274a0
Lucas Andrade <46944875+andlukass@users.noreply.github.com>
2024-05-12 22:37:10 +0100

    Delete dist directory

--- dist/parrot.sh
+++
@@ -1,9 +0,0 @@
-x=1
-while [ $x -le 99999 ]
-do 
-gnome-terminal -- sh -c "bash -c \"curl parrot.live; exec bash\""
-# osascript -e 'tell app "Terminal"
-#     do script "curl parrot.live"
-# end tell'
- x=$(( $x + 1 ))
-done
