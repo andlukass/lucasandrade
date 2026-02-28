cf50b80b8603490c567a1b6f1c62a31f26bd9bd4
Lucas Andrade <lopess.andrade@gmail.com>
2026-02-28 21:13:19 +0000

    add tokenizer

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
+import Tokenizer from './pages/Tokenizer.tsx'
 import './lib/i18n.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/tokenizer" element={<Tokenizer />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ src/pages/Tokenizer.tsx
@@ -0,0 +1,11 @@
+function Tokenizer() {
+  return (
+    <iframe
+      src="https://tokenizer-42.web.app/"
+      style={{ width: '100%', height: '100vh', border: 'none', display: 'block' }}
+      title="Tokenizer"
+    />
+  );
+}
+
+export default Tokenizer;
